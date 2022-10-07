a702a71af2ac384e42754838f8d676efc95ba110
Kaliro26 <115157729+Kaliro26@users.noreply.github.com>
2022-10-07 16:50:53 +0530

    Add files via upload

---
+++ perform all arithmetic operations on it.cpp
@@ -0,0 +1,31 @@
+// Q7. Write a program to accept two integers from the user, perform all arithmetic operations on it, and 
+// display the result
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  cout<<"enter the value of a : ";
+  cin>>a;
+  int b;
+  cout<<"enter the value of b : ";
+  cin>>b;
+  cout<<"sum of a and b is ";
+  cout<<a+b;
+  cout<<endl;
+  cout<<"multiply of a and b is ";
+  cout<<a*b;
+  cout<<endl;
+  cout<<"subtraction of a and b is ";
+  cout<<a-b;
+  cout<<endl;
+  cout<<"divison of a and b is ";
+  cout<<a/b;
+  cout<<endl;
+  cout<<"modulo of a and b is ";
+  cout<<a%b;
+  cout<<endl;
+    
+}
